bb652d562be5c57c0a15f52fc22a2ec1f3faa0a1
David Wukelic <wukebb24@hotmail.com>
2018-08-01 20:53:03 -0700

    routing is working :)

--- Week 7/TeamManager/src/app/app-routing.module.ts
+++ Week 7/TeamManager/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
   },
   {
       path: 'players',
-      component: ListComponent,
+      component: ManagePlayersComponent,
       children: [
           {
               path: '',
