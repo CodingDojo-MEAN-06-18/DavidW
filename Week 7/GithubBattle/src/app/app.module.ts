bc954fbc01da1229a09cedd9f8f00973520c7525
David Wukelic <wukebb24@hotmail.com>
2018-08-03 22:18:54 -0700

    service, components, routing module added

--- Week 7/GithubBattle/src/app/app.module.ts
+++ Week 7/GithubBattle/src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BattleComponent } from './battle/battle.component';
+import { RankingsComponent } from './rankings/rankings.component';
+import { HomeComponent } from './home/home.component';
+import { PlayerComponent } from './player/player.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BattleComponent,
+    RankingsComponent,
+    HomeComponent,
+    PlayerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
