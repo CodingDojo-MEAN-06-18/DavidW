5dccdeaf7e7d6e6d4bafaca7c398872679474910
David Wukelic <wukebb24@hotmail.com>
2018-08-05 10:09:55 -0700

    routing set up

--- Week 8/BicycleMarketplace/src/app/app-routing.module.ts
+++ Week 8/BicycleMarketplace/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { ListingsComponent } from './listings/listings.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-@NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
-})
-export class AppRoutingModule { }
+const routes: Routes = [
+    {
+        path: '', 
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent,
+    },
+    {
+        path: 'browse',
+        component: BrowseComponent,
+    },
+    {
+        path: 'listings',
+        component: ListingsComponent
+    }
+]
+export const routing = RouterModule.forRoot(routes);
--- Week 8/BicycleMarketplace/src/app/app.module.ts
+++ Week 8/BicycleMarketplace/src/app/app.module.ts
@@ -2,28 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-// import { routing } from './/app-routing.module';
+import { routing } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BrowseComponent } from './browse/browse.component';
 import { ListingsComponent } from './listings/listings.component';
 import { HttpService } from './http.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     BrowseComponent,
-    ListingsComponent
+    ListingsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    FormsModule
-    // ,routes
+    FormsModule,
+    routing
     
   ],
   providers: [HttpService],
