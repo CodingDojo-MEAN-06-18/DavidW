03f52af2c25d807868871a2d887d17264c49757c
David Wukelic <wukebb24@hotmail.com>
2018-08-04 23:26:56 -0700

    gitignore

--- Week 8/BicycleMarketplace/src/app/app.module.ts
+++ Week 8/BicycleMarketplace/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+// import { routing } from './/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { BrowseComponent } from './browse/browse.component';
 import { ListingsComponent } from './listings/listings.component';
+import { HttpService } from './http.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,13 @@ import { ListingsComponent } from './listings/listings.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+    // ,routes
+    
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
