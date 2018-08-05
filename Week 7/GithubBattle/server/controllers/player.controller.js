6ef2475dcd989f6ba7be52bcc2023a7abde8c211
David Wukelic <wukebb24@hotmail.com>
2018-08-04 20:22:51 -0700

    comments

--- Week 7/GithubBattle/server/controllers/player.controller.js
+++ Week 7/GithubBattle/server/controllers/player.controller.js
@@ -6,40 +6,15 @@ module.exports = {
     Player.find(request.body)
     .then(data => {
       response.json(data)
-      console.log('in the index method ..................................................................' )
-    })
-    .catch(error => response.json(error));
-  },
-  show(request, response) {
-    // console.log(request.params.id)
-    Player.findById(request.params.id)
-    .then(data => { 
-      // console.log('in the show method ..................................................................' )
-      // console.log(data);
-      response.json(data);
+    //   console.log('in the index method ..................................................................' )
     })
     .catch(error => response.json(error));
   },
   create(request, response) {
-    console.log('in the create method ..................................................................' )
+    // console.log('in the create method ..................................................................' )
     Player.create(request.body)
     .then(player => response.json(player))
     .catch(error => response.json(error));
-  },
-  update(request, response) {
-    // console.log('in the update method ..................................................................' )
-    Player.findByIdAndUpdate(request.params.id, request.body, { new: true })
-    .then(player => response.json(player))
-    .catch(error => response.json(error));
-  },
-  destroy(request, response) {
-    // console.log('in the delete method ..................................................................' )
-    Player.findByIdAndRemove(request.params.id)
-      .then(result => {
-        // console.log("deleting")
-        response.json(result)
-      })
-      .catch(error => response.json(error));
   },
 };
 
--- Week 7/GithubBattle/server/routes/player.routes.js
+++ Week 7/GithubBattle/server/routes/player.routes.js
@@ -4,6 +4,3 @@ const router = require('express').Router();
 module.exports = router
     .get('/getPlayers', playerController.index)
     .post('/addPlayer', playerController.create)
-    .get('/checkForUser:id', playerController.show)
-    .put('/edit/:id', playerController.update)
-    .delete('/destroy/:id', playerController.destroy);
