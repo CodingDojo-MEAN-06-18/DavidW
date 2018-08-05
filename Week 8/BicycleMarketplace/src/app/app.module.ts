d63b119af632e896e57ebbf96235fbae50116f0f
David Wukelic <wukebb24@hotmail.com>
2018-08-04 22:53:43 -0700

    components, service, routing module init

--- Week 8/BicycleMarketplace/src/app/app.module.ts
+++ Week 8/BicycleMarketplace/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { BrowseComponent } from './browse/browse.component';
+import { ListingsComponent } from './listings/listings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BrowseComponent,
+    ListingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
